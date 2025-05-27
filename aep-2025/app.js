8b5f39d9401354e081fb10563ab815eddb45f5dc
HugoLopesGM <hugolopesgarciamendes05@gmail.com>
2025-05-27 17:09:47 -0300

    js no login
    fazerLogin()
    caso o usuário não preencha todas as colunas ira mostrar uma mensagem para ele informando que não foi preenchido;
    
    se a senha 1 for diferente da senha dois ira mostrar que as senhas não se coincidem ;
    
    quando o usuário fizer o login ira mostrar um alert com uma descrição de bem vindo;

--- aep-2025/app.js
+++ aep-2025/app.js
@@ -1,5 +1,32 @@
-const app = Vue.createApp({
-    
+const { createApp } = Vue
 
-});
-app.mount ('#app')
+createApp({
+  data() {
+    return {
+      usuario: '',
+      senha: '',
+      senha2: '',
+      mensagem: ''
+    }
+  },
+  methods: {
+    fazerLogin() {
+      if (!this.usuario || !this.senha || !this.senha2) {
+        this.mensagem = 'Por favor, preencha todos os campos.'
+        return
+      }
+
+      if (this.senha !== this.senha2) {
+        this.mensagem = 'As senhas não coincidem.'
+        return
+      }
+
+      alert(`Bem-vindo, ${this.usuario}! Login realizado com sucesso.`)
+
+      this.usuario = ''
+      this.senha = ''
+      this.senha2 = ''
+      this.mensagem = ''
+    }
+  }
+}).mount('#app')
